ed5db65574393d400405462cb27d88b0d5bdac27
Masayuki Takagiwa <takagiwa@mish.co.jp>
2025-04-09 11:21:46 +0900

    Add installation of build-essentials

--- scripts/vboxguest.sh
+++ scripts/vboxguest.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-sudo mount -o loop /home/$1/VBoxGuestAdditions.iso /media/cdrom
+#ls -alF /home/$1
+sudo apt update
+sudo apt install -y gcc make perl build-essential
+sudo mkdir -p /media/cdrom
+if [ -e /home/$1/VBoxGuestAdditions.iso ]; then
+  sudo mount -o loop /home/$1/VBoxGuestAdditions.iso /media/cdrom
+elif [ -e /dev/cdrom ]; then
+  sudo mount /dev/cdrom /media/cdrom
+else
+  # not found
+  exit 1
+fi
 sudo sh /media/cdrom/VBoxLinuxAdditions.run
 sudo umount /media/cdrom
